46a50a3a5c87f09d6b10436bc6e4a9e84e228985
apelaezmun <apelaezmun1@educacion.navarra.es>
2024-11-25 13:54:36 +0100

    e

--- Ejercicio02.py
+++ Ejercicio02.py
@@ -3,9 +3,9 @@
 #mensaje “<nombre> tiene <edad> años, vive en <dirección> y su número de
 #teléfono es <teléfono>”.
 nom = input('Cual es tu nombre: ')
-años = input('Dime tu edad: ')
+anos = input('Dime tu edad: ')
 direc = input('Dame tu direccion: ')
 tlfn = input('Cual es tu numero de telefono: ')
-datos = {'Nombre':nom, 'Edad':años, 'Direccion':direc, 'Telefono':tlfn}
-print(f'{nom} tiene {años} años, vive en {direc} y su numero de telefono es 
-      {tlfn}')
+datos = {'Nombre':nom, 'Edad':anos, 'Direccion':direc, 'Telefono':tlfn}
+print(datos['Nombre'], 'tiene', datos['Edad'], 'años, vive en',
+      datos['Direccion'], 'y su numero de telefono es', datos['Telefono'])
--- Ejercicio04.py
+++ Ejercicio04.py
@@ -2,3 +2,12 @@
 #información sobre una persona (por ejemplo nombre, edad, sexo, teléfono,
 #correo electrónico, etc.) que se le pida al usuario. Cada vez que se añada un
 #nuevo dato debe imprimirse el contenido del diccionario.
+diccionario = {}
+Terminar = ''
+while Terminar != 'parar':
+    clave = input('Dame un dato a añadir: ')
+    valor = input('Dame el valor del dato: ')
+    diccionario[clave] = valor
+    print(diccionario)
+    Terminar = input('Esribir "parar" cuando no haya mas datos o pulsar enter 
+                     'si quieres continuar: ')
